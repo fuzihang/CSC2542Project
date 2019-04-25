33498ddd44a55eb144d09a4fd84baeabff35963f
zihang <fuzihang1995@gmail.com>
2019-04-25 01:08:27 -0400

    generating training data multi processing

---
+++ generate_train_data_npz.py
@@ -0,0 +1,12 @@
+import subprocess
+import multiprocessing
+
+processes = 10
+
+def work(i):
+    cmd = f'python3 rub_doom_take_cover.py {1000 * i} {1000 * (i+1)}'
+    print(cmd)
+    subprocess.call(cmd)
+
+with multiprocessing.Pool(processes) as p:
+    p.map(work, range(processes))
