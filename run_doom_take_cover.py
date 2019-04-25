4b21a24b52db8fa0aefa33d1765aa1b775843c95
zihang <fuzihang1995@gmail.com>
2019-04-25 02:06:52 -0400

    generating training data multi processing

--- run_doom_take_cover.py
+++ run_doom_take_cover.py
@@ -2,14 +2,14 @@ import numpy as np
 from vizdoom_take_cover import VizdoomTakeCover
 import os
 import sys
-start_eps = int(sys.argv[1])
-end_eps = int(sys.argv[2])
 env = VizdoomTakeCover()
-NUM_EPISODES = 200
+NUM_EPISODES = 100
 MAX_STEPS = 300
-OUT_DIR = 'VAE_take_cover_valid_data'
+OUT_DIR = 'VAE_take_cover_train_data_npz'
 t = 0
 total_step = 0
+start_eps = int(sys.argv[1])
+end_eps = int(sys.argv[2])
 
 if not os.path.exists(OUT_DIR):
     os.mkdir(OUT_DIR)
@@ -18,6 +18,7 @@ for eps in range(start_eps, end_eps):
 
     obs = []
     acs = []
+    termination = []
     o = env.reset()
     obs.append(o)
 
@@ -37,16 +38,18 @@ for eps in range(start_eps, end_eps):
 
         acs.append(action)
         obs.append(o)
-        np.save(os.path.join(OUT_DIR, f'{eps}_{t}_obs'), o)
+        termination.append(1 if d else 0)
+        # np.save(os.path.join(OUT_DIR, f'{eps}_{t}_obs'), o)
 
         if d:
             break
 
 
-    # obs = np.array(obs, dtype=np.uint8)
-    # acs = np.array(acs, dtype=np.uint8)
-    #
-    # np.savez_compressed(os.path.join(OUT_DIR, f'eps_{eps}_[{old_total_step},{total_step}]'), obs=obs, action=acs)
+    obs = np.array(obs, dtype=np.uint8)
+    acs = np.array(acs, dtype=np.uint8)
+    termination = np.array(termination, dtype=np.uint8)
+
+    np.savez_compressed(os.path.join(OUT_DIR, f'eps_{eps}]'), obs=obs, action=acs, termination=termination)
 
     total_step += 1
 
